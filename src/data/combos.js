8572472979d5df38a4d67515580b70e5c1ad72bd
Leskin Maksim <Quper2405@gmail.com>
2026-01-25 09:20:20 +0300

    add eurasia

--- src/data/combos.js
+++ src/data/combos.js
@@ -18,14 +18,30 @@ export const combos = [
     id: "quper-map",
     title: "QUPER MAP COMBO",
     preview: "img/combos/quper/quper-preview.jpg",
-    version: "1.57-4.1",
+    version_game: "1.57",
+    version: "4.1",
     date: "25.01.26",
-    video: "6UWd6wJa7s8",
+    video: "AXhZkCPflwA",
     image_top: "img/combos/quper/quper-top.jpg",
     image_bottom: "img/combos/quper/quper-bottom.jpg",
     table:
       "https://docs.google.com/spreadsheets/d/1TtDWYDt9lXQwIgxoerH5EJfgEsv6EuusAe8pYQlVdJg/edit?gid=958999320#gid=958999320",
     profile:
       "https://sharemods.com/ujp17o1hedxy/1.57_QUPER_MAP_COMBO_4.1.zip.html",
+  },
+  {
+    id: "eurasia-map",
+    title: "EURASIA MAP COMBO",
+    preview: "img/combos/eurasia/eurasia-preview.jpg",
+    version_game: "1.57",
+    version: "2.1",
+    date: "25.01.26",
+    video: "KYtOJNcVBmk",
+    image_top: "img/combos/eurasia/eurasia-top.jpg",
+    image_bottom: "img/combos/eurasia/eurasia-bottom.jpg",
+    table:
+      "https://docs.google.com/spreadsheets/d/1TtDWYDt9lXQwIgxoerH5EJfgEsv6EuusAe8pYQlVdJg/edit?gid=78135028#gid=78135028",
+    profile:
+      "https://sharemods.com/gjifdj7itlzp/1.57_EURASIA_MAP_COMBO_2.1.zip.html",
   },
 ];
