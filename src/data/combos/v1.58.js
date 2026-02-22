b348d58b7430f2c5e5f50ae48139dbb3640445a9
Leskin Maksim <Quper2405@gmail.com>
2026-02-22 19:47:02 +0300

    upd mega 2.0

--- src/data/combos/v1.58.js
+++ src/data/combos/v1.58.js
@@ -5,15 +5,15 @@ export const combos_v1_58 = [
     title: "MEGA MAP COMBO",
     preview: "img/combos/158/ets2-mega/mega-preview.jpg",
     version_game: "1.58",
-    version: "1.2",
+    version: "2.0",
     date: "22.02.26",
     video: "oxTMNZk9FWk",
     image_top: "img/combos/158/ets2-mega/mega-top.jpg",
     image_bottom: "img/combos/158/ets2-mega/mega-bottom.jpg",
     table:
-      "https://docs.google.com/spreadsheets/d/1jRDPO0Mhepox1kFe2sslD9Ac_6NCelo-t770o25bFKU/edit?gid=71162410#gid=71162410",
+      "https://docs.google.com/spreadsheets/d/1jRDPO0Mhepox1kFe2sslD9Ac_6NCelo-t770o25bFKU/edit?gid=925159654#gid=925159654",
     profile:
-      "https://sharemods.com/fnpftt814wrw/1.58.MEGA_MAP_COMBO_1.2.zip.html",
+      "https://sharemods.com/hx1bcjpnlv1l/1.58.MEGA_MAP_COMBO_2.0.zip.html",
   },
   {
     id: "eurasia-map-158",
