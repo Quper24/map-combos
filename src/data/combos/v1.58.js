1beaf34adb4b62da305da3ab301fa0f31ee40411
Leskin Maksim <Quper2405@gmail.com>
2026-03-02 22:10:53 +0300

    update mods

--- src/data/combos/v1.58.js
+++ src/data/combos/v1.58.js
@@ -115,14 +115,13 @@ export const combos_v1_58 = [
     title: "ETS2 MODS COMBO",
     preview: "img/combos/158/ets2-mods/ets2-mods-preview.jpg",
     version_game: "1.58",
-    version: "1.2",
-    date: "21.02.26",
+    version: "1.3",
+    date: "02.03.26",
     video: "oxTMNZk9FWk",
     image_center: "img/combos/158/ets2-mods/ets2-mods-center.jpg",
     table:
       "https://docs.google.com/spreadsheets/d/1jRDPO0Mhepox1kFe2sslD9Ac_6NCelo-t770o25bFKU/edit?gid=1202853076#gid=1202853076",
-    profile:
-      "https://sharemods.com/1hraj0wgom4g/1.58_MODS_PACK_1.2-100.zip.html",
+    profile: "https://sharemods.com/9dg623m0ae1b/1.58_MODS_PACK_1.3.zip.html",
   },
 
   {
