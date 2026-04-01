187b6e6e84ae8871d12d2b4a69ab3b93391cc6c1
Leskin Maksim <Quper2405@gmail.com>
2026-04-02 02:13:36 +0300

    upd mega 4.3

--- src/data/combos/v1.58.js
+++ src/data/combos/v1.58.js
@@ -5,7 +5,7 @@ export const combos_v1_58 = [
     title: "MEGA MAP COMBO",
     preview: "img/combos/158/ets2-mega/mega-preview.jpg",
     version_game: "1.58",
-    version: "4.2",
+    version: "4.3",
     date: "02.04.26",
     video: "z_nIg8glATQ",
     image_top: "img/combos/158/ets2-mega/mega-top.jpg",
@@ -13,7 +13,7 @@ export const combos_v1_58 = [
     // table:
     //   "https://docs.google.com/spreadsheets/d/1jRDPO0Mhepox1kFe2sslD9Ac_6NCelo-t770o25bFKU/edit?gid=1631626983#gid=1631626983",
     profile:
-      "https://sharemods.com/m3efo0hlmtov/1.58_ETS2_MEGA_MAP_COMBO_4.2.zip.html",
+      "https://sharemods.com/e98kjhawhd7k/1.58_ETS2_MEGA_MAP_COMBO_4.3.zip.html",
   },
   {
     id: "quper-map-158",
