1ba617787da84d12848b0df764a96a7efd69f7e1
Leskin Maksim <Quper2405@gmail.com>
2026-04-08 09:13:21 +0300

    upd

--- src/data/combos/v1.58.js
+++ src/data/combos/v1.58.js
@@ -177,7 +177,7 @@ export const combos_v1_58 = [
     preview: "img/combos/158/ats-server/ats-server-preview.jpg",
     version_game: "1.58",
     version: "server",
-    date: "27.03.26",
+    date: "08.04.26",
     video: "G4ADUHZ1ruI",
     image_center: "img/combos/158/ats-server/ats-server-center.jpg",
     profile: "https://sharemods.com/lpv1v4tmx693/ATS_SERVER_PROFILE.zip.html",
