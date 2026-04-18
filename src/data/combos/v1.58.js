4ae49fddf911cc65bb4b96ac25aef6137b6fcf43
Maksim Leskin <quper2405@gmail.com>
2026-04-18 19:24:11 +0300

    update ats server

--- src/data/combos/v1.58.js
+++ src/data/combos/v1.58.js
@@ -177,10 +177,10 @@ export const combos_v1_58 = [
     preview: "img/combos/158/ats-server/ats-server-preview.jpg",
     version_game: "1.58",
     version: "server",
-    date: "13.04.26",
+    date: "18.04.26",
     video: "G4ADUHZ1ruI",
     image_center: "img/combos/158/ats-server/ats-server-center.jpg",
-    profile: "https://sharemods.com/lpv1v4tmx693/ATS_SERVER_PROFILE.zip.html",
+    profile: "https://sharemods.com/e9spxshd051d/ATS_SERVER.zip.html",
   },
   {
     id: "ets2-server-map-158",
