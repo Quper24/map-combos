dedbdc1d1257aaa166b884a2e66a3bac0ad7e0b7
Maksim Leskin <Quper2405@gmail.com>
2026-05-12 15:20:22 +0300

    add gold

--- src/data/combos/v1.59.js
+++ src/data/combos/v1.59.js
@@ -1,17 +1,32 @@
 export const combos_v1_59 = [
+  {
+    id: "gold-russia-map-159",
+    tags: ["ets2", "boosty", "convoy"],
+    title: "GOLD RUSSIA MAP COMBO",
+    preview: "img/combos/159/ets2-gold/gold-russia-preview.jpg",
+    version_game: "1.59",
+    version: "0.1",
+    date: "12.05.26",
+    video: "z_nIg8glATQ",
+    image_top: "img/combos/159/ets2-gold/gold-russia-top.jpg",
+    image_bottom: "img/combos/159/ets2-gold/gold-russia-bottom.jpg",
+    table:
+      "",
+    profile: "https://sharemods.com/wb85tnpzmi19/1.59_GoldMapCombo_0.1.zip.html",
+  },
  {
     id: "no-promods-map-159",
     tags: ["ets2", "boosty"],
     title: "NO PROMODS MAP COMBO",
     preview: "img/combos/159/ets2-no-promods/no-promods-preview.jpg",
     version_game: "1.59",
-    version: "0.1",
+    version: "0.2",
     date: "12.05.26",
     video: "z_nIg8glATQ",
     image_top: "img/combos/159/ets2-no-promods/no-promods-top.jpg",
     image_bottom: "img/combos/159/ets2-no-promods/no-promods-bottom.jpg",
     table:
       "",
-    profile: "https://sharemods.com/rm3wjedb363u/NoPromodsCombo_0.1.zip.html",
+    profile: "https://sharemods.com/bg22c7xuzu06/NoPromodsCombo_0.2.zip.html",
   },
 ];
