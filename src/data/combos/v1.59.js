326181f55c3097a2ae54ba0991939e7d697504fc
Maksim Leskin <quper2405@gmail.com>
2026-05-31 17:12:04 +0300

    upd

--- src/data/combos/v1.59.js
+++ src/data/combos/v1.59.js
@@ -86,7 +86,7 @@ export const combos_v1_59 = [
     date: "27.05.26",
     image_center:
       "img/combos/159/ets2-server-light/ets2-server-light-center.jpg",
-    profile: "https://disk.yandex.ru/d/y3bnuPCnXGFQCw",
+    profile: "https://disk.yandex.ru/d/6CjeZS9OhcM-pg",
     table:
       "https://docs.google.com/spreadsheets/d/1zOdNqLie6WgdvBxh0r2UkEyEUszB6e6NxWfHAue5X8c/edit?gid=2132576498#gid=2132576498",
   },
