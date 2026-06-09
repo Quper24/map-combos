41e168a4326e12b16e702d0328522ecfb4a1d00b
Maksim Leskin <quper2405@gmail.com>
2026-06-10 01:05:32 +0300

    upd mega

--- src/data/combos/v1.59.js
+++ src/data/combos/v1.59.js
@@ -13,7 +13,7 @@ export const combos_v1_59 = [
     table:
       "https://docs.google.com/spreadsheets/d/1zOdNqLie6WgdvBxh0r2UkEyEUszB6e6NxWfHAue5X8c/edit?gid=1631626983#gid=1631626983",
     profile:
-      "https://sharemods.com/kcvgse69y6vb/1.59_ETS2_MEGA_MAP_COMBO_1.2.3.zip.html",
+      "https://sharemods.com/kcvgse69y6vb/1.59_ETS2_MEGA_MAP_COMBO_1.2.4.zip.html",
   },
   {
     id: "quper-map-159",
