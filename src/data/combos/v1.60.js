24f6b513c763c4c586e8374566bd0691e1a00472
Maksim Leskin <quper2405@gmail.com>
2026-06-21 02:04:34 +0300

    upd gold

--- src/data/combos/v1.60.js
+++ src/data/combos/v1.60.js
@@ -1,6 +1,22 @@
 export const combos_v1_60 = [
 
   {
+    id: "gold-russia-map-160",
+    tags: ["ets2", "convoy", "boosty"],
+    title: "GOLD RUSSIA MAP COMBO",
+    preview: "img/combos/160/ets2-gold/gold-russia-preview.jpg",
+    version_game: "1.60",
+    version: "1.0",
+    date: "20.06.26",
+    video: "RXls5u4_v7c",
+    image_top: "img/combos/160/ets2-gold/gold-russia-top.jpg",
+    image_bottom: "img/combos/160/ets2-gold/gold-russia-bottom.jpg",
+    table:
+      "",
+    profile:
+      "https://sharemods.com/ddtwh95q21j7/1.60_ETS2_GOLD_MAP_COMBO_1.0.zip.html",
+  },
+  {
     id: "ats-quper-map-160",
     tags: ["ats", "convoy", "boosty"],
     title: "ATS QUPER COMBO",
