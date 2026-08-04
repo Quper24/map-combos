605a9ba9255053c7e3a1db506d865c89afc39267
Maksim Leskin <quper2405@gmail.com>
2026-08-04 22:20:14 +0300

    upd ats

--- src/data/combos/v1.60.js
+++ src/data/combos/v1.60.js
@@ -112,12 +112,12 @@ export const combos_v1_60 = [
     image_top: "img/combos/160/ats-quper/ats-quper-top.jpg",
     image_bottom: "img/combos/160/ats-quper/ats-quper-bottom.jpg",
     version_game: "1.60",
-    version: "2.1",
-    date: "24.07.26",
+    version: "2.2",
+    date: "04.08.26",
     video: "_jA5ygN1jaI",
     table: "",
     profile:
-      "https://sharemods.com/1fua21heutrl/1.60_ATS_QUPER_MAP_COMBO_2.1.zip.html",
+      "https://sharemods.com/c95jcd5nuljz/1.60_ATS_QUPER_MAP_COMBO_2.2.zip.html",
   },
   {
     id: "ats-server-map-160",
