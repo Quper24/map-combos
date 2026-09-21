edf08598738fd22bdd62cb860d7596a2dd131723
Quper <Quper2405@gmail.com>
2026-09-21 09:52:25 +0300

    upd

--- src/data/combos/v1.61.js
+++ src/data/combos/v1.61.js
@@ -7,14 +7,14 @@ export const combos_v1_61 = [
     automods: true,
     //mods: true,
     version_game: "1.61",
-    version: "1.2",
-    date: "19.09.26",
+    version: "1.3",
+    date: "21.09.26",
     //video: "b7eAwWEoPxg",
     image_top: "img/combos/161/ets2-mega-boosty/mega-boosty-top.jpg",
     image_bottom: "img/combos/161/ets2-mega-boosty/mega-boosty-bottom.jpg",
     table:
       "https://boosty.to/qupersimulator/posts/227e6b8e-edfe-40d3-aaff-4593d4447947",
     profile:
-      "https://sharemods.com/4jd7xgdapxra/1.61_ETS2_START_COMBO_MAP_1.2.zip.html",
+      "https://sharemods.com/xqaij1kyjqus/1.61_ETS2_START_COMBO_MAP_1.3.zip.html",
   },
 ];
