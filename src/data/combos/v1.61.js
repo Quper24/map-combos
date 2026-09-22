30f5d8a76eb8bfd2a17bb04cb9c3618402f335e4
Quper <Quper2405@gmail.com>
2026-09-22 03:22:17 +0300

    upd

--- src/data/combos/v1.61.js
+++ src/data/combos/v1.61.js
@@ -2,7 +2,7 @@ export const combos_v1_61 = [
   {
     id: "mega-boosty-map-161",
     tags: ["ets2", "boosty"],
-    title: "START BOOSTY MAP COMBO",
+    title: "MEGA BOOSTY MAP COMBO",
     preview: "img/combos/161/ets2-mega-boosty/mega-boosty-preview.jpg",
     automods: true,
     //mods: true,
@@ -16,5 +16,23 @@ export const combos_v1_61 = [
       "https://boosty.to/qupersimulator/posts/227e6b8e-edfe-40d3-aaff-4593d4447947",
     profile:
       "https://sharemods.com/xqaij1kyjqus/1.61_ETS2_START_COMBO_MAP_1.3.zip.html",
+  },
+  {
+    id: "mega-map-161",
+    tags: ["ets2"],
+    title: "MEGA MAP COMBO",
+    preview: "img/combos/161/ets2-mega-boosty/mega-boosty-preview.jpg",
+    automods: true,
+    //mods: true,
+    version_game: "1.61",
+    version: "1.3",
+    date: "22.09.26",
+    //video: "b7eAwWEoPxg",
+    image_top: "img/combos/161/ets2-mega-boosty/mega-boosty-top.jpg",
+    image_bottom: "img/combos/161/ets2-mega-boosty/mega-boosty-bottom.jpg",
+    table:
+      "https://docs.google.com/spreadsheets/d/1VFj7KxcfsTVbuHMkHR9KKn_Zb8qDGuMSzEfDV3icBs4/edit?usp=sharing",
+    profile:
+      "https://sharemods.com/bavgqbukpk8y/1.61_ETS2_MEGA_COMBO_MAP_1.3.zip.html",
   },
 ];
