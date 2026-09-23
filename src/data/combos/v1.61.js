5352992ff6ad5eb7a3f2c4e56492a3e71f651259
Quper <Quper2405@gmail.com>
2026-09-23 14:04:40 +0300

    upd

--- src/data/combos/v1.61.js
+++ src/data/combos/v1.61.js
@@ -61,13 +61,13 @@ export const combos_v1_61 = [
     automods: true,
     //mods: true,
     version_game: "1.61",
-    version: "23.09.26",
-    // date: "23.09.26",
+    version: "1.0",
+    date: "23.09.26",
     // video: "RXls5u4_v7c",
     image_top: "img/combos/161/ets2-gold/gold-russia-top.jpg",
     image_bottom: "img/combos/161/ets2-gold/gold-russia-bottom.jpg",
-    // table:
-    //   "https://docs.google.com/spreadsheets/d/1JqBp4yAnQ2Tl-nWjD8kQZe4pwvKkaHr0O9yYPNa2gXk/edit?gid=1078812500#gid=1078812500",
+    table:
+      "https://docs.google.com/spreadsheets/d/1VFj7KxcfsTVbuHMkHR9KKn_Zb8qDGuMSzEfDV3icBs4/edit?gid=1593428445#gid=1593428445",
     profile:
       "https://sharemods.com/k71reihji85c/1.61_ETS2_GOLD_COMBO_MAP_1.0.zip.html",
   },
